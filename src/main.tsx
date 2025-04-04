d04b46d0c7b7ea08c06a1b73bca84fe087e5866d
bribella-pl <admbribella@gmail.com>
2025-04-05 00:45:54 +0200

    add tailwindcss, font and global css with colors

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Buffer } from "buffer";
 import App from "./App.tsx";
+import "./index.css";
 
 window.Buffer = Buffer;
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig(() => {
-  const plugins = [react()];
+  const plugins = [tailwindcss(), react()];
 
   if (process.env.TEST_ENV === "true") {
     plugins.push(
