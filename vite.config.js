e4bba577673430740ac3dedce67e438b50c25abd
bribella-pl <admbribella@gmail.com>
2025-04-28 00:39:32 +0200

    update tsconfig

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { plugin as markdownPlugin, Mode } from "vite-plugin-markdown";
+import path from "path";
+export default defineConfig(() => {
+    const plugins = [
+        tailwindcss(),
+        react(),
+        markdownPlugin({
+            mode: [Mode.MARKDOWN],
+        }),
+    ];
+    return {
+        plugins,
+        resolve: {
+            alias: {
+                "@content": path.resolve(__dirname, "content"),
+            },
+        },
+    };
+});
