9abbebd004c85f19ae99fb5392883b28b64b74c1
bribella-pl <admbribella@gmail.com>
2025-05-23 08:54:13 +0200

    move script to body

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,23 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { plugin as markdownPlugin, Mode } from "vite-plugin-markdown";
 import path from "path";
+
 export default defineConfig(() => {
-    const plugins = [
-        tailwindcss(),
-        react(),
-        markdownPlugin({
-            mode: [Mode.MARKDOWN],
-        }),
-    ];
-    return {
-        base: "/bribella-react",
-        plugins,
-        resolve: {
-            alias: {
-                "@content": path.resolve(__dirname, "content"),
-            },
-        },
-    };
+  const plugins = [
+    tailwindcss(),
+    react(),
+    markdownPlugin({
+      mode: [Mode.MARKDOWN],
+    }),
+  ];
+
+  return {
+    base: "/bribella-react",
+    plugins,
+    resolve: {
+      alias: {
+        "@content": path.resolve(__dirname, "content"),
+      },
+    },
+  };
 });
