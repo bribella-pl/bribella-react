e2db1d96ea0b17c9f05581b5c8816c1946e26df9
bribella-pl <bribella-pl@users.noreply.github.com>
2025-05-28 06:45:02 +0000

    Updates

---
+++ admin/config.js
@@ -0,0 +1,441 @@
+const config = {
+  "backend": {
+    "name": "github",
+    "repo": "bribella-pl/bribella-react",
+    "branch": "main",
+    "base_url": "https://auth-provider-bribella-pls-projects.vercel.app",
+    "auth_endpoint": "/api/oauth"
+  },
+  "media_folder": "public/images",
+  "public_folder": "/images",
+  "collections": [
+    {
+      "name": "quotesSection",
+      "label": "Cytaty",
+      "folder": "content/quotes",
+      "create": true,
+      "slug": "{{fields.author}}-{{year}}-{{month}}-{{day}}",
+      "fields": [
+        {
+          "label": "Autor",
+          "name": "author",
+          "widget": "string"
+        },
+        {
+          "label": "Cytat",
+          "name": "quote",
+          "widget": "text"
+        }
+      ]
+    },
+    {
+      "name": "homeSection",
+      "label": "Strona główna",
+      "folder": "content/home",
+      "create": false,
+      "slug": "{{slug}}",
+      "fields": [
+        {
+          "label": "Główne zdjęcie",
+          "name": "mainImageUrl",
+          "widget": "image"
+        },
+        {
+          "label": "Sekcja 1 – tytuł",
+          "name": "section1Title",
+          "widget": "string"
+        },
+        {
+          "label": "Sekcja 1 – treść",
+          "name": "section1Text",
+          "widget": "string"
+        },
+        {
+          "label": "Drugie zdjęcie",
+          "name": "secondImageUrl",
+          "widget": "image"
+        },
+        {
+          "label": "Sekcja 2 – tytuł",
+          "name": "section2Title",
+          "widget": "string"
+        },
+        {
+          "label": "Sekcja 2 – treść",
+          "name": "section2Text",
+          "widget": "string"
+        },
+        {
+          "label": "Trzecie zdjęcie",
+          "name": "thirdImageUrl",
+          "widget": "image"
+        }
+      ]
+    },
+    {
+      "name": "newsSection",
+      "label": "Aktualności",
+      "folder": "content/news/simple",
+      "create": false,
+      "slug": "{{slug}}",
+      "fields": [
+        {
+          "label": "Tytuł sekcji",
+          "name": "title",
+          "widget": "string"
+        },
+        {
+          "label": "Ilość wpisów na stronie",
+          "name": "itemsPerPage",
+          "widget": "string"
+        },
+        {
+          "label": "Treść sekcji",
+          "name": "content",
+          "widget": "markdown"
+        },
+        {
+          "label": "Główne zdjęcie",
+          "name": "mainImageUrl",
+          "widget": "image"
+        }
+      ]
+    },
+    {
+      "name": "newsPosts",
+      "label": "Aktualności – wpisy",
+      "folder": "content/news",
+      "create": true,
+      "slug": "{{year}}-{{month}}-{{day}}-{{fields.title}}",
+      "fields": [
+        {
+          "label": "Data wpisu",
+          "name": "date",
+          "widget": "datetime"
+        },
+        {
+          "label": "Tytuł wpisu",
+          "name": "title",
+          "widget": "string"
+        },
+        {
+          "label": "Treść wpisu",
+          "name": "content",
+          "widget": "markdown"
+        },
+        {
+          "name": "images",
+          "label": "Zdjęcia",
+          "widget": "list",
+          "allow_add": true,
+          "field": {
+            "label": "Obrazek",
+            "name": "imageUrl",
+            "widget": "image"
+          }
+        }
+      ]
+    },
+    {
+      "name": "studsSection",
+      "label": "Kocury",
+      "files": [
+        {
+          "label": "Opis kocurów",
+          "name": "studsPage",
+          "file": "content/cats/studs.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Treść",
+              "name": "content",
+              "widget": "markdown"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "studsPosts",
+      "label": "Kocury – wpisy",
+      "folder": "content/cats/studs",
+      "create": true,
+      "slug": "{{year}}-{{month}}-{{day}}-{{fields.name}}",
+      "fields": [
+        {
+          "label": "Data ur.",
+          "name": "date",
+          "widget": "datetime"
+        },
+        {
+          "label": "Imię",
+          "name": "name",
+          "widget": "string"
+        },
+        {
+          "label": "Opis",
+          "name": "content",
+          "widget": "markdown"
+        },
+        {
+          "name": "images",
+          "label": "Zdjęcia",
+          "widget": "list",
+          "allow_add": true,
+          "field": {
+            "label": "Obrazek",
+            "name": "imageUrl",
+            "widget": "image"
+          }
+        }
+      ]
+    },
+    {
+      "name": "queensSection",
+      "label": "Kotki",
+      "files": [
+        {
+          "label": "Opis kotek",
+          "name": "queensPage",
+          "file": "content/cats/queens.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Treść",
+              "name": "content",
+              "widget": "markdown"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "queensPosts",
+      "label": "Kotki – wpisy",
+      "folder": "content/cats/queens",
+      "create": true,
+      "slug": "{{year}}-{{month}}-{{day}}-{{fields.name}}",
+      "fields": [
+        {
+          "label": "Data ur.",
+          "name": "date",
+          "widget": "datetime"
+        },
+        {
+          "label": "Imię",
+          "name": "name",
+          "widget": "string"
+        },
+        {
+          "label": "Opis",
+          "name": "content",
+          "widget": "markdown"
+        },
+        {
+          "name": "images",
+          "label": "Zdjęcia",
+          "widget": "list",
+          "allow_add": true,
+          "field": {
+            "label": "Obrazek",
+            "name": "imageUrl",
+            "widget": "image"
+          }
+        }
+      ]
+    },
+    {
+      "name": "kittensSection",
+      "label": "Kocięta",
+      "files": [
+        {
+          "label": "Opis kociąt",
+          "name": "kittensPage",
+          "file": "content/cats/kittens.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "kittensPosts",
+      "label": "Kocięta – wpisy",
+      "folder": "content/cats/kittens",
+      "create": true,
+      "slug": "{{year}}-{{month}}-{{day}}-{{fields.name}}",
+      "fields": [
+        {
+          "label": "Data ur.",
+          "name": "date",
+          "widget": "datetime"
+        },
+        {
+          "label": "Imię",
+          "name": "name",
+          "widget": "string"
+        },
+        {
+          "label": "Opis",
+          "name": "content",
+          "widget": "markdown"
+        },
+        {
+          "label": "Miot",
+          "name": "litter",
+          "widget": "string"
+        },
+        {
+          "name": "images",
+          "label": "Zdjęcia",
+          "widget": "list",
+          "allow_add": true,
+          "field": {
+            "label": "Obrazek",
+            "name": "imageUrl",
+            "widget": "image"
+          }
+        }
+      ]
+    },
+    {
+      "name": "aboutSection",
+      "label": "O nas",
+      "files": [
+        {
+          "label": "Strona O nas",
+          "name": "aboutPage",
+          "file": "content/about/about.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Treść",
+              "name": "content",
+              "widget": "markdown"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "contactSection",
+      "label": "Kontakt",
+      "files": [
+        {
+          "label": "Strona Kontakt",
+          "name": "contactPage",
+          "file": "content/contact/contact.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Treść",
+              "name": "content",
+              "widget": "markdown"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "beforeBuySection",
+      "label": "Zanim kupisz",
+      "files": [
+        {
+          "label": "Strona Zanim kupisz",
+          "name": "beforeBuyPage",
+          "file": "content/before-you-buy/before-you-buy.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Treść",
+              "name": "content",
+              "widget": "markdown"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "notFoundSection",
+      "label": "404 Strona",
+      "files": [
+        {
+          "label": "Strona nie znaleziona",
+          "name": "notFoundPage",
+          "file": "content/not-found/not-found.md",
+          "fields": [
+            {
+              "label": "Tytuł",
+              "name": "title",
+              "widget": "string"
+            },
+            {
+              "label": "Treść",
+              "name": "content",
+              "widget": "markdown"
+            },
+            {
+              "label": "Zdjęcie główne",
+              "name": "mainImageUrl",
+              "widget": "image"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
